325c2f24df769ad8d47cd08ec2408cc006f9005b
Muhammed Nafees <muh.nafees909@gmail.com>
2024-01-14 19:38:30 -0800

    simple front-end implemented

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import '../styles/HomePage.css';
 
 const HomePage = () => {
     return(
-        <div>
-            <h1>Home</h1>
+        <div className='homepage-container'>
+            <h1 className='homepage-header'>Task Manager</h1>
+            <button className='new-task-btn'>New Task +</button>
+            <div className='outer-flexbox'>
+                <div className='inner-flexbox'>
+                    <div className='task-header'>Todo</div>
+                    <div className='flex-item'>
+                        <div className='task-date'>
+                            Today*
+                        </div>
+                        <div className='task-name'>
+                            Finish this project
+                        </div>
+                        <div className='task-time'>
+                            9:00 AM - 12:00 PM
+                        </div>
+                    </div>
+                    <div className='flex-item'>todo 2</div>
+                </div>
+                <div className='inner-flexbox'>
+                    <div className='task-header'>In Progress</div>
+                    <div className='flex-item'>in progress 1</div>
+                </div>
+                <div className='inner-flexbox'>
+                    <div className='task-header'>Completed</div>
+                    <div className='flex-item'>completed</div>
+                    <div className='flex-item'>completed</div>
+                    <div className='flex-item'>completed</div>
+                </div>
+            </div>
         </div>
     )
 };
