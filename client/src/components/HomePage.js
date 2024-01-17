309acca6828fe1a1b3ecdd902d912d98a46528d0
Muhammed Nafees <muh.nafees909@gmail.com>
2024-01-16 20:44:45 -0800

    added styling, added basic todo functionality

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,40 +1,144 @@
-import React from 'react';
-import '../styles/HomePage.css';
+import React from "react";
+import "../styles/HomePage.css";
+import TaskForm from "./TaskForm";
+import { useState } from "react";
 
 const HomePage = () => {
-    return(
-        <div className='homepage-container'>
-            <h1 className='homepage-header'>Task Manager</h1>
-            <button className='new-task-btn'>New Task +</button>
-            <div className='outer-flexbox'>
-                <div className='inner-flexbox'>
-                    <div className='task-header'>Todo</div>
-                    <div className='flex-item'>
-                        <div className='task-date'>
-                            Today*
-                        </div>
-                        <div className='task-name'>
-                            Finish this project
-                        </div>
-                        <div className='task-time'>
-                            9:00 AM - 12:00 PM
-                        </div>
-                    </div>
-                    <div className='flex-item'>todo 2</div>
-                </div>
-                <div className='inner-flexbox'>
-                    <div className='task-header'>In Progress</div>
-                    <div className='flex-item'>in progress 1</div>
-                </div>
-                <div className='inner-flexbox'>
-                    <div className='task-header'>Completed</div>
-                    <div className='flex-item'>completed</div>
-                    <div className='flex-item'>completed</div>
-                    <div className='flex-item'>completed</div>
-                </div>
+  const [formButton, setFormButton] = useState(false);
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  return (
+    <div className="homepage-container">
+      <div className="header">
+        <h1 className="homepage-header">TaskMate</h1>
+      </div>
+      <button className="new-task-btn" onClick={() => setFormButton(true)}>
+        New Task +
+      </button>
+      <div className="outer-flexbox">
+        <div className="inner-flexbox">
+          <div className="task-header">Todo</div>
+
+          <div className="task-list">
+            {todos.map((todo, index) => (
+              <div key={index} className="flex-item">
+                <div className="task-date">Today*</div>
+                <div className="task-name">{todo}</div>
+                <div className="task-time">9:00 AM - 12:00 PM</div>
+              </div>
+            ))}
+
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
             </div>
+          </div>
         </div>
-    )
+        <div className="inner-flexbox">
+          <div className="task-header">In Progress</div>
+          <div className="task-list">
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+          </div>
+        </div>
+        <div className="inner-flexbox">
+          <div className="task-header">Completed</div>
+          <div className="task-list">
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+          </div>
+        </div>
+        <div className="inner-flexbox">
+          <div className="task-header">Overdue</div>
+          <div className="task-list">
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+            <div className="flex-item">
+              <div className="task-date">Today*</div>
+              <div className="task-name">Finish this project</div>
+              <div className="task-time">9:00 AM - 12:00 PM</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <TaskForm
+        trigger={formButton}
+        setTrigger={setFormButton}
+        onAddTodo={handleAddTodo}
+      >
+        <h3>Form</h3>
+      </TaskForm>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ client/src/components/TaskForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+import "../styles/TaskForm.css";
+
+function TaskForm(props) {
+  const [newTodo, setNewTodo] = useState([]);
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+        props.onAddTodo(newTodo);
+        setNewTodo('');
+      }
+  };
+  return props.trigger ? (
+    <div className="task-form-container">
+      <div className="form-content">
+        <h1 className="task-form-title">Add a New Task</h1>
+        <button
+          className="close-button"
+          onClick={() => props.setTrigger(false)}
+        >
+          Close
+        </button>
+        {props.children}
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add new todo"
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+    </div>
+  ) : (
+    ""
+  );
+}
+
+export default TaskForm;
